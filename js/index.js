c5cd68ef3bfa06689acaea58a850e25e35453a44
Yuliia <yuliyatimosh96@ukr.net>
2019-02-07 13:49:21 +0000

    created calculator

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const link = document.querySelector('a');
+
+
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits, .opers')
+    .forEach(btn => btn.addEventListener('click', digitOperPressed));
+
+function digitOperPressed(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.result')
+    .addEventListener('click', function() {
+        display.value = eval(display.value);
+    })
